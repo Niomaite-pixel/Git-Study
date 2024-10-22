6b1fd09e03bef457d0126feca43c979465171551
Karina <nssnhdfj@gmail.com>
2024-10-22 12:54:13 +1000

    Добавлены исключения

--- ЛР1/ЛР1/Program.cs
+++ ЛР1/ЛР1/Program.cs
@@ -11,84 +11,95 @@ namespace ЛР1
         static string loginData = "login_Data.txt";
         static void Main(string[] args)
         {
-            while(true)
+			string username;
+			string password;
+			Console.WriteLine("Пожалуйста, введите имя пользователя:");
+			username = Console.ReadLine();
+			Console.WriteLine("Пожалуйста, введите пароль:");
+			password = Console.ReadLine();
+			StartLogin(username, password);
+            while (true)
             {
-                string username;
-                string password;
                 int usersDigits;
                 int machineDigits = Rnd();
-				string machineStr = machineDigits.ToString();
-                int[] machineArray = new int [4];
+                string machineStr = machineDigits.ToString();
+                int[] machineArray = new int[4];
 				bool newGame = false;
                 int attempts = 0;
                 int choice;
                 bool flag = true;
 
-
-				for (int i = 0; i < machineStr.Length; i++)
+                for (int i = 0; i < machineStr.Length; i++)
                 {
                     machineArray[i] = int.Parse(machineStr[i].ToString());
-				}
-
-                Console.WriteLine("Пожалуйста, введите имя пользователя:");
-                username = Console.ReadLine();
-                Console.WriteLine("Пожалуйста, введите пароль:");
-                password = Console.ReadLine();
-                while (Login(username, password) == false)
-                {
-                    System.Console.WriteLine("Имя пользователя или пароль введены неверно. Попробуйте еще раз");
-                    Console.WriteLine("Пожалуйста, введите имя пользователя:");
-                    username = Console.ReadLine();
-                    Console.WriteLine("Пожалуйста, введите пароль:");
-                    password = Console.ReadLine();
                 }
-                Console.WriteLine("Авторизация прошла успешно");
-                Console.WriteLine("Машина загадала четырёхзначный код. Цифры в одном коде не повторяются.");
-                while(newGame == false)
+
+
+				Console.WriteLine("Машина загадала четырёхзначный код. Цифры в одном коде не повторяются.");
+                Console.WriteLine(machineDigits);
+                while (newGame == false)
                 {
                     Console.Write("Ваш вариант:");
-                    usersDigits = Convert.ToInt32(Console.ReadLine());
-
-                    Compare(usersDigits, machineArray, out int isCount, out int posCount);
-                    attempts++;
-                    Console.WriteLine("Вы угадали цифр: " + isCount);
-                    Console.WriteLine("Количество цифр на своих местах: " + posCount);
-                    if(isCount == 4 && posCount == 4)
+                    try
                     {
-                        newGame = true;
-                        Log(attempts);
-                        UpdateBestResult(attempts);
+                        usersDigits = Convert.ToInt32(Console.ReadLine());
+                        if (usersDigits.ToString().Length > 4)
+                        {
+                            Console.WriteLine(("Введено больше четырёх символов!"));
+                        }
+                        Compare(usersDigits, machineArray, out int isCount, out int posCount);
+                        attempts++;
+                        Console.WriteLine("Вы угадали цифр: " + isCount);
+                        Console.WriteLine("Количество цифр на своих местах: " + posCount);
+                        if (isCount == 4 && posCount == 4)
+                        {
+                            newGame = true;
+                            Log(attempts, username);
+                            UpdateBestResult(attempts, username);
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Буквы и другие символы в коде недопустимы!");
                     }
                 }
                 while (flag == true)
                 {
-                    Console.WriteLine("Желаете продолжить? \n1.Да \n2.Нет \n3.Посмотреть наилучший результат");
+                    Console.WriteLine("Желаете продолжить? \n1.Да \n2.Нет \n3.Посмотреть наилучший результат \n4.Сменить пользователя");
                     choice = Convert.ToInt32(Console.ReadLine());
                     if (choice == 1)
                     {
                         flag = false;
                         continue;
                     }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine("Выход из программы...");
+                        Environment.Exit(0);
+                    }
                     else if (choice == 3)
                     {
                         DisplayBestResult(); // Отображение лучшего результата
                         continue;
                     }
-                    else if (choice == 2)
+                    else if (choice == 4)
                     {
-                        Console.WriteLine("Выход из программы...");
-                        Environment.Exit(0); 
-                    }
+						Console.WriteLine("Пожалуйста, введите имя пользователя:");
+						username = Console.ReadLine();
+						Console.WriteLine("Пожалуйста, введите пароль:");
+						password = Console.ReadLine();
+						StartLogin(username, password);
+					}
                     else
                     {
                         Console.WriteLine("Некорректный ввод, попробуйте снова.");
                     }
-                }                
+                }
             }
         }
 
-        public static int Rnd() 
-        { 
+        public static int Rnd()
+        {
             int[] digits = new int[4];
             int res = 0;
             Random rnd = new Random();
@@ -100,21 +111,21 @@ namespace ЛР1
                 do
                 {
                     digit = rnd.Next(0, 10);
-                } while (usedDigits.Contains(digit)); 
+                } while (usedDigits.Contains(digit));
 
                 digits[i] = digit;
-                usedDigits.Add(digit); 
+                usedDigits.Add(digit);
             }
 
-            foreach(int dig in digits)
+            foreach (int dig in digits)
             {
-                res =  res * 10 + dig;
+                res = res * 10 + dig;
             }
             return res;
         }
-        public static void Log(int attempts)
+        public static void Log(int attempts, string username)
         {
-            string logText = $"Победа достигнута за {attempts} попыток в {DateTime.Now}\n";
+            string logText = $"Победа достигнута за {attempts} попыток в {DateTime.Now} - пользователь {username}\n";
 
             try
             {
@@ -127,7 +138,7 @@ namespace ЛР1
             }
         }
 
-        public static void UpdateBestResult(int attempts)
+        public static void UpdateBestResult(int attempts, string username)
         {
             string bestResultLine;
             if (File.Exists(logFile))
@@ -138,7 +149,7 @@ namespace ЛР1
                     // Если текущий результат лучше, обновляем файл
                     if (attempts < bestAttempts)
                     {
-                        File.WriteAllLines(logFile, new[] { $"Лучший результат: {attempts} попыток" });
+                        File.WriteAllLines(logFile, new[] { $"Лучший результат: {attempts} попыток - пользователь {username}" });
                         Console.WriteLine("Лучший результат обновлен.");
                     }
                 }
@@ -146,16 +157,17 @@ namespace ЛР1
             else
             {
                 // Если файл не существует, создаем его с текущим результатом
-                File.WriteAllLines(logFile, new[] { $"Лучший результат: {attempts} попыток" });
-            }
-        }
+                File.WriteAllLines(logFile, new[] { $"Лучший результат: {attempts} попыток - пользователь {username}" });
+		    	File.AppendAllText(logFile, $"Победа достигнута за {attempts} попыток в {DateTime.Now} - пользователь {username}\n");
+			}
+		}
 
         public static void DisplayBestResult()
         {
             if (File.Exists(logFile))
             {
                 string bestResultLine = File.ReadAllLines(logFile)[0];
-                Console.WriteLine("Лучший результат: " + bestResultLine);
+                Console.WriteLine(bestResultLine);
             }
             else
             {
@@ -164,14 +176,22 @@ namespace ЛР1
         }
         public static void Compare(int usersDigits, int[] arrMachineDigits, out int isCount, out int posCount)
         {
-			string usersStr = usersDigits.ToString();
-			int[] usersArray = new int[4];
-			for (int i = 0; i < usersStr.Length; i++)
-			{
-				usersArray[i] = int.Parse(usersStr[i].ToString());
+            string usersStr = usersDigits.ToString();
+            int[] usersArray = new int[4];
+            try
+            {
+			    for (int i = 0; i < usersStr.Length; i++)
+                {
+                    usersArray[i] = int.Parse(usersStr[i].ToString());
+                }
+            }
+            catch (Exception)
+            {
+				Console.WriteLine("Код состоит ровно из четырёх цифр! Буквы и другие символы недопустимы!");
+			
 			}
 
-			int a = usersDigits / 1000;
+            int a = usersDigits / 1000;
             int b = usersDigits % 1000 / 100;
             int c = usersDigits % 100 / 10;
             int d = usersDigits % 10;
@@ -187,23 +207,35 @@ namespace ЛР1
             {
                 if (arrMachineDigits[i] == usersArray[i])
                 {
-					isCount++;
-					posCount++;
+                    isCount++;
+                    posCount++;
                 }
                 else if (!allSameDigits && arrMachineDigits.Contains(usersArray[i]) && !countedDigits.Contains(usersArray[i]))
                 {
                     isCount++;
-                    countedDigits.Add(usersArray[i]); 
+                    countedDigits.Add(usersArray[i]);
                 }
                 else if (allSameDigits && arrMachineDigits.Contains(a) && !countedDigits.Contains(a))
                 {
-                    isCount = 1; 
+                    isCount = 1;
                     posCount = 1;
-                    countedDigits.Add(a); 
-                    break; 
+                    countedDigits.Add(a);
+                    break;
                 }
             }
         }
+        public static void StartLogin(string username, string password)
+        { 
+			while (Login(username, password) == false)
+			{
+				System.Console.WriteLine("Имя пользователя или пароль введены неверно. Попробуйте еще раз");
+				Console.WriteLine("Пожалуйста, введите имя пользователя:");
+				username = Console.ReadLine();
+				Console.WriteLine("Пожалуйста, введите пароль:");
+				password = Console.ReadLine();
+			}
+			Console.WriteLine("Авторизация прошла успешно");
+		}
         public static bool Login(string username, string password)
         {
             if (File.Exists(loginData))
